3746965136784aa264f8ba9ff1224fac48e24788
Samuel Tenschert <samuel.tenschert@gmail.com>
2026-05-18 17:08:41 +0200

    feat: find X in single-row map

--- src/help_alf.js
+++ src/help_alf.js
@@ -5,4 +5,12 @@ HelpAlf.prototype.findSpaceship = function(map) {
     if (map === '' || map.indexOf('X') === -1) {
         return 'Spaceship lost forever.';
     }
+    var rows = map.split('\n');
+    for (var i = 0; i < rows.length; i++) {
+        var x = rows[i].indexOf('X');
+        if (x !== -1) {
+            var y = (rows.length - 1) - i;
+            return [x, y];
+        }
+    }
 };
