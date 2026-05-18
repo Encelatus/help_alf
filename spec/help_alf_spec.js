2cc716e3aff8c3fc386c01766870e340d241c547
Samuel Tenschert <samuel.tenschert@gmail.com>
2026-05-18 17:03:05 +0200

    test: add failing test for missing spaceship

--- spec/help_alf_spec.js
+++ spec/help_alf_spec.js
@@ -1,9 +1,14 @@
-describe('Help ALF', function() {
-    beforeEach(function() {
-        finder = new HelpAlf();
-    });
+describe("Help ALF", function () {
+  beforeEach(function () {
+    finder = new HelpAlf();
+  });
 
-    it('should return "Spaceship lost forever." for an empty map', function() {
-        expect(finder.findSpaceship('')).toEqual('Spaceship lost forever.');
-    });
+  it('should return "Spaceship lost forever." for an empty map', function () {
+    expect(finder.findSpaceship("")).toEqual("Spaceship lost forever.");
+  });
+  it('should return "Spaceship lost forever." when X is not in the map', function () {
+    expect(finder.findSpaceship(".........")).toEqual(
+      "Spaceship lost forever.",
+    );
+  });
 });
